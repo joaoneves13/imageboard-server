a87125132858f64fdacf396522d3b69bcac81b98
João Neves <13joaoneves@gmail.com>
2019-12-06 10:26:13 +0100

    creating database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const db = require('./db')
 
 const app = express()
 const port = process.env.PORT || 4000
+
 
 app.listen(port, console.log(`Listening on: ${port}`))
