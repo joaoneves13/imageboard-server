31d537381c78dea6450d86bf2af9dab3a227501f
João Neves <13joaoneves@gmail.com>
2019-12-06 11:29:43 +0100

    data(images)

--- image/router.js
+++ image/router.js
@@ -1,12 +1,17 @@
 const { Router } = require('express')
 const Image = require('./model')
-
 const router = new Router()
 
 router.get('/image', (req, res) => {
     Image.findAll()
     .then(images => res.send(images))
     .catch(console.error)
+})
+
+router.post('/image', (req, res, next) => {
+    Image.create(req.body)
+    .then(image => res.json(image))
+    .catch(err => next(err))
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const imageRouter = require('./image/router')
-const app = express()
 const port = process.env.PORT || 4000
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+
+const parserMiddleware = bodyParser.json()
+app.use(parserMiddleware)
 
 app.use(imageRouter)
+
 app.listen(port, console.log(`Listening on: ${port}`))
